d33f04b3843c158278f34b0f5dbdf24bdd4f8d20
Layta <130605457+LLayta@users.noreply.github.com>
2023-08-27 21:38:38 -0500

    Delete target directory

--- target/debug/deps/dot_testing-4ed545574200f329.d
+++
@@ -1,5 +0,0 @@
-/home/layta/programming/dot_testing/target/debug/deps/dot_testing-4ed545574200f329: src/main.rs
-
-/home/layta/programming/dot_testing/target/debug/deps/dot_testing-4ed545574200f329.d: src/main.rs
-
-src/main.rs:
--- target/debug/dot_testing.d
+++
@@ -1,1 +0,0 @@
-/home/layta/programming/dot_testing/target/debug/dot_testing: /home/layta/programming/dot_testing/src/main.rs
